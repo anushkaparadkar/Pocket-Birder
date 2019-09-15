6ad24d4aac9ef23a4d27a43a89281df0f708f38e
Anushka Paradkar <anupara15@gmail.com>
2019-09-15 18:48:13 +0530

    layout of features page

---
+++ lib/views/add-image.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker_modern/image_picker_modern.dart';
+
+class ImagePickerExample extends StatefulWidget {
+  @override
+  _ImagePickerExampleState createState() => _ImagePickerExampleState();
+}
+
+class _ImagePickerExampleState extends State<ImagePickerExample> {
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _image == null ? Text('No image selected.') : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+      
+    );
+  }
+}
